25115c2ecb75e761e21e067d19e2e2a5ac1c3a33
Rohit Dalal <7984480+rd67@users.noreply.github.com>
2024-02-18 18:05:10 +0530

    Updated Minor COnfig

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.0
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/jaswdr/faker/v2 v2.0.3
-	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.18.2
 	github.com/stretchr/testify v1.8.4
--- util/config.go
+++ util/config.go
@@ -2,51 +2,81 @@ package util
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/spf13/viper"
 )
 
 type Config struct {
 	APP_ENV string `mapstructure:"APP_ENV"`
-	PORT    string    `mapstructure:"PORT"`
+	PORT    string `mapstructure:"PORT"`
 
 	DB_DRIVER   string `mapstructure:"DB_DRIVER"`
 	DB_HOST     string `mapstructure:"DB_HOST"`
-	DB_PORT     string    `mapstructure:"DB_PORT"`
+	DB_PORT     string `mapstructure:"DB_PORT"`
 	DB_USER     string `mapstructure:"DB_USER"`
 	DB_NAME     string `mapstructure:"DB_NAME"`
 	DB_PASSWORD string `mapstructure:"DB_PASSWORD"`
 
-	DB_URL string
+	DB_URL string `mapstructure:"DB_URL"`
 }
 
+// LoadConfig reads configuration from file or environment variables.
 func LoadConfig(path string) (config Config, err error) {
+	viper.SetConfigName(".env")
+	viper.SetConfigType("dotenv")
 	viper.AddConfigPath(path)
-	viper.SetConfigFile(".env")
 
 	err = viper.ReadInConfig()
 	if err != nil {
-		log.Fatal("Can't find the file .env : ", err)
 		return
 	}
+
+	viper.AutomaticEnv()
 
 	err = viper.Unmarshal(&config)
-	if err != nil {
-		log.Fatal("Environment can't be loaded: ", err)
-		return
+
+	if len(config.DB_URL) == 0 {
+		//	Updating DB URL if not present in the .env
+		config.DB_URL = fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable",
+			config.DB_DRIVER,
+			config.DB_USER,
+			config.DB_PASSWORD,
+			config.DB_HOST,
+			config.DB_PORT,
+			config.DB_NAME,
+		)
 	}
 
-	log.Printf("The App is running in %s env", config.APP_ENV)
+	return
 
-	config.DB_URL = fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable",
-		config.DB_DRIVER,
-		config.DB_USER,
-		config.DB_PASSWORD,
-		config.DB_HOST,
-		config.DB_PORT,
-		config.DB_NAME,
-	)
+	// viper.AddConfigPath(path)
+	// viper.SetConfigFile(".env")
 
-	return config, err
+	// err = viper.ReadInConfig()
+	// if err != nil {
+	// 	log.Fatal("Can't find the file .env : ", err)
+	// 	return
+	// }
+
+	// err = viper.Unmarshal(&config)
+	// if err != nil {
+	// 	log.Fatal("Environment can't be loaded: ", err)
+	// 	return
+	// }
+
+	// log.Printf("The App is running in %s env", config.APP_ENV)
+
+	// if config.DB_URL == "" {
+	// 	//	Updating DB URL if not present in the .env
+	// 	config.DB_URL = fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable",
+	// 		config.DB_DRIVER,
+	// 		config.DB_USER,
+	// 		config.DB_PASSWORD,
+	// 		config.DB_HOST,
+	// 		config.DB_PORT,
+	// 		config.DB_NAME,
+	// 	)
+	// }
+
+	// return config, err
 }
